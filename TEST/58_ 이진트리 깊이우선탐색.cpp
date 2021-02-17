e86fa619141671a4194e603eda07e1d184af1757
hyee0715 <hyee0715@naver.com>
2021-02-17 21:47:10 +0900

    재귀 & 깊이/넓이 우선탐색(DFS, BFS)

--- TEST/58_ 이진트리 깊이우선탐색.cpp
+++ TEST/58_ 이진트리 깊이우선탐색.cpp
@@ -8,9 +8,20 @@ void recursive(int n) {
 		return;
 	}
 	else {
+		// 전위순회 출력 : 1 2 4 5 3 6 7 
 		cout << n << " ";
 		recursive(n * 2);
 		recursive(n * 2 + 1);
+
+		// 중위순회 출력 : 4 2 5 1 6 3 7
+//		recursive(n * 2);
+//		cout << n << " ";
+//		recursive(n * 2 + 1);
+
+		// 후위순회 출력 : 4 5 2 6 7 3 1
+//		recursive(n * 2);
+//		recursive(n * 2 + 1);
+//		cout << n << " ";
 	}
 }
 
